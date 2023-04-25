b4b6e6771935079dc29f3aecbeacff78ee81dfe2
KayKilb <infulbloom@msn.com>
2023-04-24 20:08:40 -0700

    fixed syntax issues

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,22 +1,23 @@
 #include "binary_trees.h"
 /**
-* binary_tree_node - creates a binary tree node
-* @value: Integer stored in the node
-* @parent: Pointer to the parent node
-* Return: pointer to new node
-*/
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+ * binary_tree_node - creates a binary tree node
+ * @value: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * Return: pointer to new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-binary_tree_t *newNode;
-newNode = malloc(sizeof(binary_tree_t));
+    binary_tree_t *newNode;
+    newNode = malloc(sizeof(binary_tree_t));
 
-if (newNode == NULL)
-	return (NULL);
+    if (newNode == NULL)
+        return (NULL);
 
-newNode->parent = parent;
-newNode->n = value;
-newNode->left = NULL;
-newNode->right = NULL;
+    newNode->parent = parent;
+    newNode->n = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
 
-return (newNode);
+    return (newNode);
 }
+
